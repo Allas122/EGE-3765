d5a4873bfd0bfda23453468eb10b4be7db82bb4c
Alias <Error122122122122122@yandex.ru>
2023-12-16 21:35:15 +0800

    kek

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,24 +1,24 @@
 ﻿
-Dictionary<int,int> prostDel(int n)
+Dictionary<int,int> Factorization(int n)
 {
-    var o =new Dictionary<int,int>();
-    for (int i = 2; i <= Math.Sqrt(n)+1; i++)
+    var Factorization =new Dictionary<int,int>();
+    for (int Dels = 2; i <= Math.Sqrt(n)+1; i++)
     {
-        if (!o.ContainsKey(i) && n % i ==0)
+        if (!Factorization.ContainsKey(i) && n % Dels ==0)
         {
-            o[i] = 0;
+            Factorization[Dels] = 0;
         }
-        while(n % i == 0)
+        while(n % Dels == 0)
         {
-            n /= i;
-            o[i] += 1;
+            n /= Dels;
+            Factorization[Dels] += 1;
         }
     }
     if (n != 1)
     {
-        o[n]= 1;
+        Factorization[n]= 1;
     }
-    return o;
+    return Factorization;
 }
 
 
@@ -27,26 +27,23 @@ int N(int k) {
 }
 
 
-int sumOfDels(Dictionary<int,int> pr){
-    int o = 1;
-    int un = 1;
-    foreach(int i in pr.Keys)
+int CountDels(Dictionary<int,int> Factorization){
+    int Dels = 1;
+    int DelsWithoutTwo = 1;
+    foreach(int i in Factorization.Keys)
     {
         if(i != 2)
         {
-            un *= (pr[i] + 1);
+            DelsWithoutTwo *= (pr[i] + 1);
         }
-        o *= (pr[i] + 1);
+        Dels *= (Factorization[i] + 1);
     }
-    return o - un;
+    return Dels - DelsWithoutTwo;
 }
 
 for(int i = 0; i <=1000000 ; i+=2)
 {
-    var pr = prostDel(N(i));
-    var s = sumOfDels(pr);
-    if (s % 2 != 0)
-    {
-        Console.WriteLine(i.ToString() + " " + s);
-    }
+    var factorization = Factorization(N(i));
+    var s = CountDels(factorization);
+    Console.WriteLine(i.ToString() + " " + s);
 }
